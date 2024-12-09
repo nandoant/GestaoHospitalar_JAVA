8f189205bf7067583e5fc48553827d932ffb0a7b
nandoant <fernandoantonio@protonmail.com>
2024-12-09 09:46:58 -0300

    retirando os statusPaciente do consulta

--- GestaoHospitalar/src/gestaohospitalar/model/Consulta.java
+++ GestaoHospitalar/src/gestaohospitalar/model/Consulta.java
@@ -5,16 +5,12 @@ public class Consulta {
     Paciente paciente;
     Medico medico;
     String descricao;
-    PacienteStatus statusAtualPaciente;
-    PacienteStatus statusAnteriorPaciente;
 
-    public Consulta(int id, Paciente paciente, Medico medico, String descricao, PacienteStatus statusAtualPaciente, PacienteStatus statusAnteriorPaciente) {
+    public Consulta(int id, Paciente paciente, Medico medico, String descricao) {
         this.id = id;
         this.paciente = paciente;
         this.medico = medico;
         this.descricao = descricao;
-        this.statusAtualPaciente = statusAtualPaciente;
-        this.statusAnteriorPaciente = statusAnteriorPaciente;
     }
 
     public int getId() {
@@ -50,20 +46,9 @@ public class Consulta {
     }
 
     public PacienteStatus getStatusAtualPaciente() {
-        return statusAtualPaciente;
+        return this.paciente.getStatusAtual();
     }
-
-    public void setStatusAtualPaciente(PacienteStatus statusAtualPaciente) {
-        this.statusAtualPaciente = statusAtualPaciente;
-    }
-
-    public PacienteStatus getStatusAnteriorPaciente() {
-        return statusAnteriorPaciente;
-    }
-
-    public void setStatusAnteriorPaciente(PacienteStatus statusAnteriorPaciente) {
-        this.statusAnteriorPaciente = statusAnteriorPaciente;
-    }
+    
     
     
 }

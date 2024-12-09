ca513501e30c56521907598da551d9e690b2da50
nandoant <fernandoantonio@protonmail.com>
2024-12-09 15:49:45 -0300

    criacao do metodo lerInteiro

--- GestaoHospitalar/src/gestaohospitalar/Utils/ConsoleUI.java
+++ GestaoHospitalar/src/gestaohospitalar/Utils/ConsoleUI.java
@@ -1,8 +1,43 @@
 package gestaohospitalar.Utils;
 
-public class ConsoleUI {
+import java.util.Scanner;
 
+public class ConsoleUI {
     public static void clear(){
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    public static int lerInteiro(String mensagem){
+        Scanner scanner = new Scanner(System.in);
+        int valor = 0;
+        boolean valido = false;
+        do {
+            System.out.print(mensagem);
+            try {
+                valor = Integer.parseInt(scanner.nextLine());
+                valido = true;
+            } catch (Exception e) {
+                System.out.println("Valor inválido. Tente novamente.");
+            }
+        } while (!valido);
+        scanner.close();
+        return valor;
+    }
+
+    public static int lerInteiro(){
+        Scanner scanner = new Scanner(System.in);
+        int valor = 0;
+        boolean valido = false;
+        do {
+            try {
+                valor = Integer.parseInt(scanner.nextLine());
+                valido = true;
+            } catch (Exception e) {
+                System.out.println("Valor inválido. Tente novamente.");
+            }
+        } while (!valido);
+
+        scanner.close();
+        return valor;
     }
 }
